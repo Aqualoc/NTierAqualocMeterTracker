b1603e9382aabeb984cce37094ab199bdd9d5e51
Ahmed Vadia <ayvadia@gmail.com>
2018-03-06 13:22:54 +0200

    FIxed formatting in login class

--- PresentationTier/FormLogin.vb
+++ PresentationTier/FormLogin.vb
@@ -1,29 +1,17 @@
-﻿
-
-
-Public Class frmLogin
+﻿Public Class frmLogin
     Dim Val As String = String.Empty
-
     Private Sub cmbLoginOk_Click(sender As Object, e As EventArgs) Handles cmbLoginOk.Click
         Dim SecondForm As New frmScanning
-
-
         SecondForm.TxtScanningOperator.Text = Val
-
         If txtLoginPassword.Text = "a" And
             cboLogin.SelectedIndex <> -1 And
             cboLoginStation.SelectedIndex <> -1 Then
-
             SecondForm.Show()
             Me.Hide()
-
-
         Else
             If txtLoginPassword.Text = "" Then
                 MsgBox("No Password found!", MsgBoxStyle.Critical, "Error")
             Else
-
-
                 If cboLogin.SelectedItem = "" Then
                     MsgBox("No Login Selected!", MsgBoxStyle.Critical, "Error")
                 Else
@@ -36,91 +24,48 @@ Public Class frmLogin
 
                         Else
                             MsgBox("Invalid Password!", MsgBoxStyle.Critical, "Error")
-
-
                         End If
-
-
                     End If
-
-
-
                 End If
-
-
             End If
         End If
-
-
     End Sub
-
-
-
-
 
     Private Sub cmbLoginCancel_Click(sender As Object, e As EventArgs) Handles cmbLoginCancel.Click
         End
-
-
     End Sub
 
     Private Sub cboLogin_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboLogin.SelectedIndexChanged
         If cboLogin.SelectedItem = "Thabo" Then
-
         Else
             If cboLogin.SelectedItem = "Morena" Then
-
             Else
                 If cboLogin.SelectedItem = "Rachel" Then
-
                 Else
                     If cboLogin.SelectedItem = "Preshen" Then
                     Else cboLogin.SelectedItem = "Ahmed"
-
-
-
                     End If
-
                 End If
-
-
             End If
-
-
         End If
     End Sub
 
     Private Sub cboLoginStation_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboLoginStation.SelectedIndexChanged
         If cboLoginStation.SelectedItem = "Test1" Then
-
         Else
             If cboLoginStation.SelectedItem = "Test2" Then
-
             Else
                 If cboLoginStation.SelectedItem = "Test3" Then
-
                 Else
                     If cboLoginStation.SelectedItem = "Test4" Then
-
                     Else cboLoginStation.SelectedItem = "Test5"
-
-
-
-
                     End If
-
                 End If
-
-
             End If
-
-
         End If
     End Sub
 
     Private Sub cboLogin_SelectedValueChanged(sender As Object, e As EventArgs) Handles cboLogin.SelectedValueChanged
-
-
         Val = cboLogin.SelectedItem
     End Sub
 
@@ -128,10 +73,7 @@ Public Class frmLogin
         cboLogin.ResetText()
         cboLoginStation.ResetText()
         txtLoginPassword.ResetText()
-
         cboLogin.Focus()
-
-
     End Sub
 
     Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
